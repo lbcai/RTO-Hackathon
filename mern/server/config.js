3f987222ad1457e6220902227e5c60eeca34a840
L Cai <lizbcai@gmail.com>
2022-07-23 19:18:59 -0700

    Fix MongoDB connection

---
+++ mern/server/config.js
@@ -0,0 +1,8 @@
+// config.js
+
+let config = {
+    dbUrl:
+      "mongodb+srv://User:N3yFxy2GVcHdac9w@cluster0.vxssvs3.mongodb.net/hack?retryWrites=true&w=majority",
+  };
+
+  module.exports = config;
--- mern/server/db/conn.js
+++ mern/server/db/conn.js
@@ -1,12 +1,13 @@
 const { MongoClient } = require("mongodb");
-const Db = process.env.ATLAS_URI;
+const config = require("../config");
+const Db = config.dbUrl;
 const client = new MongoClient(Db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
- 
+
 var _db;
- 
+
 module.exports = {
   connectToServer: function (callback) {
     client.connect(function (err, db) {
@@ -14,12 +15,12 @@ module.exports = {
       if (db)
       {
         _db = db.db("hack");
-        console.log("Successfully connected to MongoDB."); 
+        console.log("Successfully connected to MongoDB.");
       }
       return callback(err);
          });
   },
- 
+
   getDb: function () {
     return _db;
   },
