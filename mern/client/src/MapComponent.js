e0bfcf61592e0ea60c643f9ef722d583c2be93e3
L Cai <lizbcai@gmail.com>
2022-07-23 17:20:20 -0700

    Iterative display of info windows.

--- mern/client/src/MapComponent.js
+++ mern/client/src/MapComponent.js
@@ -11,6 +11,13 @@ const center = {
     lng: -73.9976128429272
 };
 
+const locationArray = [
+    {lat: 40.738833492365465,
+    lng: -73.9976128429272},
+    {lat: 40.7388335,
+    lng: -73.9977}
+];
+
 const divStyle = {
   background: `white`,
   border: `1px solid #ccc`,
@@ -28,13 +35,16 @@ function MapComponent() {
         zoom={19}
       >
 
+    {locationArray.map((value, index) => (
         <InfoWindow
-        position={center}
+        position={value}
+        key={index}
         >
         <div style={divStyle}>
-            <h1>InfoWindow</h1>
+            <h1>test</h1>
         </div>
         </InfoWindow>
+    ))}
 
       </GoogleMap>
     </LoadScript>
